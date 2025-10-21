f0746a0f47508a1cb0fb11082590ff85d5f0a14b
Jayprakash Shukla <jayshukla29.13@gmail.com>
2025-10-21 14:08:52 +0530

    updated flask app

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask
-from pymongo import MongoClient
+from sqlalchemy import create_engine
 import os
 
 app = Flask(__name__)
 
-mongo_uri = os.environ.get("MONGO_URI")
-client = MongoClient(mongo_uri)
-db = client.get_database()
+database_url = os.environ.get("DATABASE_URL")
+engine = create_engine(database_url)
 
 @app.route("/")
 def home():
-    return "Hello from Flask connected to MongoDB!"
+    return "Hello from Flask connected to PostgreSQL!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
